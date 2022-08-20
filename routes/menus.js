58602ab3862438816c703022b6d343c78e21167b
Ories <tellt@126.com>
2022-08-20 11:36:48 +0800

    8-7 菜单列表递归实现（下）完成

--- routes/menus.js
+++ routes/menus.js
@@ -14,36 +14,28 @@ router.get('/list', async (ctx) => {
   // const permissionList = util.getTreeMenu(rootList, null, [])
   const permissionList = getTreeMenu(rootList, null, [])
 
-  ctx.body = util.success(rootList)
+  ctx.body = util.success(permissionList)
 })
 
 // 递归拼接树形列表
 function getTreeMenu(rootList, id, list) {
   for (let i = 0; i < rootList.length; i++) {
     let item = rootList[i]
-    if (item.parentId.pop() === id) {
-      console.log('item => ', item)
-      list.push(item)
+    if (String(item.parentId.slice().pop()) === String(id)) {
+      list.push(item._doc)
     }
-    // if (String(item.parentId.slice().pop()) == String(id)) {
-    //   list.push(item._doc)
-    // }
   }
   list.map(item => {
     item.children = []
-    getTreeMenu(rootList, item._id)
+    getTreeMenu(rootList, item._id, item.children)
+    if (item.children.length === 0) {
+      delete item.children
+    } else if (item.children.length > 0 && item.children[0].menuType === 2) {
+      // 快速区分按钮和菜单，用于后期做菜单按钮权限控制
+      item.action = item.children
+    }
   })
-//   list.map((item) => {
-//     item.children = []
-//     getTreeMenu(rootList, item._id, item.children)
-//     if (item.children.length == 0) {
-//       delete item.children
-//     } else if (item.children.length > 0 && item.children[0].menuType == 2) {
-//       // 快速区分按钮和菜单，用于后期做菜单按钮权限控制
-//       item.action = item.children
-//     }
-//   })
-//   return list
+  return list
 }
 
 // 菜单编辑、删除、新增功能
